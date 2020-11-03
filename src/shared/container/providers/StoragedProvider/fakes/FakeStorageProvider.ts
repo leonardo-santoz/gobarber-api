c7bf8da044fce7969854272543c27cbdc8cc378c
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2020-11-02 21:36:41 -0300

    02 - Aplicando TDD na pratica

--- src/shared/container/providers/StoragedProvider/fakes/FakeStorageProvider.ts
+++ src/shared/container/providers/StoragedProvider/fakes/FakeStorageProvider.ts
@@ -12,7 +12,7 @@ class FakeStorageProvider implements IStoragedProvider {
 
     public async deleteFile(file: string): Promise<void> {
         const findIndex = this.storage.findIndex(storageFile => storageFile === file);
-        
+
         this.storage.splice(findIndex, 1);
     }
 }
